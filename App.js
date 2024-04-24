d4635725734ab13e359a377499c8944bc448e32d
shivansh806 <144509294+shivansh806@users.noreply.github.com>
2024-04-25 01:20:24 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,18 @@
+
+import React from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+// import LoginPage from 'C:\Users\HP\Downloads\OpenBook-main\OpenBook-main\OpenBook-main\src\Components\LoginPage.js';
+import Main from './Components/Main';
+// import './Components/Login/basicsform';
+// import BasicForm from './Components/Login/basicsform';
+// import RegisterForm from './RegisterForm';
+
+function App() {
+  return (
+     <div>
+      <Main/>
+     </div>
+  );
+}
+
+export default App;
